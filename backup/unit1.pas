695f6e80ff0cf517b3ed052ca677a117f40a0760
Paul <paul.fictor@utt.fr>
2018-05-23 11:35:51 +0200

    start side bar

---
+++ backup/unit1.pas
@@ -0,0 +1,32 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  Menus, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ComboBox1: TComboBox;
+    Shape1: TShape;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
--- unit1.pas
+++ unit1.pas
@@ -5,10 +5,20 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  Menus, StdCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    ComboBox1: TComboBox;
+    ComboBox2: TComboBox;
+    ComboBox3: TComboBox;
+    ComboBox4: TComboBox;
+    ComboBox5: TComboBox;
+    Shape1: TShape;
   private
 
   public
