36e1bbf4c8ece1c1a94b7215329248aaad82a3ce
Paul <paul.fictor@utt.fr>
2018-05-24 17:20:17 +0200

    seance projet paul 1

--- unit1.pas
+++ unit1.pas
@@ -19,11 +19,17 @@ type
     ComboBox4: TComboBox;
     ComboBox5: TComboBox;
     Label1: TLabel;
+    ListeSemestre: TRadioGroup;   //Liste semestre
+    ListeUETC: TRadioGroup;   //Liste UE TC
     Shape1: TShape;
     Shape2: TShape;
     Shape3: TShape;
-    procedure Label1Click(Sender: TObject);
-    procedure StaticText1Click(Sender: TObject);
+    procedure ComboBox1Change(Sender: TObject);
+    procedure ComboBox2Change(Sender: TObject);
+    procedure ComboBox3Change(Sender: TObject);
+    procedure ComboBox4Change(Sender: TObject);
+    procedure ComboBox5Change(Sender: TObject);
+
   private
 
   public
@@ -39,14 +45,83 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.StaticText1Click(Sender: TObject);
-begin
-
+procedure TForm1.ComboBox1Change(Sender: TObject);
+begin //GESTION UE
+  case ComboBox1.ItemIndex of
+       0: begin
+          ListeUETC.Visible:=True ;
+          ListeSemestre.Visible:=False; //TC
+       end ;
+       1: ; //ISI
+       2: ; //RT
+       3: ; //A2I
+       4: ; //MM
+       5: ; //MTE
+       6: ; //GM
+       7: ; //GI
+  end;
 end;
 
-procedure TForm1.Label1Click(Sender: TObject);
-begin
+procedure TForm1.ComboBox2Change(Sender: TObject);
+begin  //GESTION ENSEIGNANTS
+    case ComboBox1.ItemIndex of
+       0: ; //TC
+       1: ; //ISI
+       2: ; //RT
+       3: ; //A2I
+       4: ; //MM
+       5: ; //MTE
+       6: ; //GM
+       7: ; //GI
+  end;
+end;
 
+procedure TForm1.ComboBox3Change(Sender: TObject);
+begin  //VISUALISATION REPARTITION ETU
+    ListeSemestre.Visible:= True;
+    ListeUETC.Visible:=False;
+    case ComboBox1.ItemIndex of
+       0: ; //TC
+       1: ; //ISI
+       2: ; //RT
+       3: ; //A2I
+       4: ; //MM
+       5: ; //MTE
+       6: ; //GM
+       7: ; //GI
+  end;
+end;
+
+procedure TForm1.ComboBox4Change(Sender: TObject);
+begin  //GESTION DU SEMESTRE
+    ListeSemestre.Visible:= True;
+    ListeUETC.Visible:=False;
+    case ComboBox1.ItemIndex of
+       0: ; //TC
+       1: ; //ISI
+       2: ; //RT
+       3: ; //A2I
+       4: ; //MM
+       5: ; //MTE
+       6: ; //GM
+       7: ; //GI
+  end;
+end;
+
+procedure TForm1.ComboBox5Change(Sender: TObject);
+begin  //STATISTIQUE DE FLUX
+    ListeSemestre.Visible:= True;
+    ListeUETC.Visible:=False;
+    case ComboBox1.ItemIndex of
+       0: ; //TC
+       1: ; //ISI
+       2: ; //RT
+       3: ; //A2I
+       4: ; //MM
+       5: ; //MTE
+       6: ; //GM
+       7: ; //GI
+  end;
 end;
 
 end.
