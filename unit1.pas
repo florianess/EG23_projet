2db614db5d6abc741893e247d5eb44c31c34bf63
Florian Esslinger <florian.esslinger@live.fr>
2018-06-12 00:56:32 +0200

    refactor menu

--- unit1.pas
+++ unit1.pas
@@ -13,11 +13,24 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    A2Ibu: TRadioButton;
+    GestEN: TButton;
+    GestUE: TButton;
+    GIbu: TRadioButton;
+    GMbu: TRadioButton;
+    GroupBox1: TGroupBox;
+    ISIbu: TRadioButton;
+    MTEbu: TRadioButton;
+    RepEtu: TButton;
+    GestSeme: TButton;
+    RTbu: TRadioButton;
+    StatsFlux: TButton;
     ComboBox1: TComboBox;
     ComboBox2: TComboBox;
     ComboBox3: TComboBox;
     ComboBox4: TComboBox;
     ComboBox5: TComboBox;
+    TCbu: TRadioButton;
     TriStatistique: TComboBox;
     Image4: TImage;
     Image5: TImage;
@@ -60,7 +73,22 @@ type
     procedure ComboBox3Change(Sender: TObject);
     procedure ComboBox4Change(Sender: TObject);
     procedure ComboBox5Change(Sender: TObject);
+    procedure GestSemeClick(Sender: TObject);
+    procedure GestUEClick(Sender: TObject);
+    procedure GIbuChange(Sender: TObject);
+    procedure GMbuChange(Sender: TObject);
+    procedure GroupBox1ChangeBounds(Sender: TObject);
+    procedure GroupBox1Click(Sender: TObject);
+    procedure Image7Click(Sender: TObject);
+    procedure ISIbuChange(Sender: TObject);
+    procedure ListeUEA2IClick(Sender: TObject);
     procedure ListeUEISIClick(Sender: TObject);
+    procedure NameFluxClick(Sender: TObject);
+    procedure RepEtuClick(Sender: TObject);
+    procedure RTbuChange(Sender: TObject);
+    procedure A2IbuChange(Sender: TObject);
+    procedure StatsFluxClick(Sender: TObject);
+    procedure TCbuChange(Sender: TObject);
 
   private
 
@@ -71,6 +99,7 @@ type
 var
   Form1: TForm1;
   indexSemestre : integer;
+  cursus: String;
 
 implementation
 
@@ -97,8 +126,6 @@ begin
      Form1.StatistiqueFlux.Visible:=False;
 
 end;
-
-
 
 procedure TForm1.ComboBox1Change(Sender: TObject); //GESTION UE
 begin
@@ -149,7 +176,46 @@ begin
   end;
 end;
 
+procedure TForm1.NameFluxClick(Sender: TObject);
+begin
 
+end;
+
+procedure TForm1.RepEtuClick(Sender: TObject);
+begin
+  Clear();
+    case cursus of
+       'ISI': RepartitionISI.Visible:=True; //ISI
+       else ShowMessage('Nous n avons pas encore d informations sur la répartition demandée');
+  end;
+end;
+
+procedure TForm1.RTbuChange(Sender: TObject);
+begin
+    if Form1.RTbu.Checked then cursus:='RT';
+     if Form1.MTEbu.Checked then cursus:='MTE';
+     if Form1.GMbu.Checked then cursus:='GM';
+     if Form1.GIbu.Checked then cursus:='GI';
+end;
+
+procedure TForm1.A2IbuChange(Sender: TObject);
+begin
+     if Form1.A2Ibu.Checked then cursus:='A2I';
+end;
+
+procedure TForm1.StatsFluxClick(Sender: TObject);
+begin
+  Clear();
+    case cursus of
+       'ISI': StatistiqueFlux.Visible:=True; //ISI
+       else ShowMessage('Nous n avons pas encore les statistiques demandées');
+  end
+end;
+
+procedure TForm1.TCbuChange(Sender: TObject);
+begin
+   if TCbu.Checked then cursus:='TC';
+end;
 
 procedure TForm1.ComboBox2Change(Sender: TObject); //GESTION ENSEIGNANTS
 begin
@@ -221,5 +287,84 @@ begin
        else ShowMessage('Nous n avons pas encore les statistiques demandées');
   end;
 end;
+
+procedure TForm1.GestSemeClick(Sender: TObject);
+begin
+  Clear();
+  case cursus of
+       'ISI': GestionEtudiantISI.Visible:=True ; //ISI
+       else ShowMessage('Nous n avons pas encore d informations sur la gestion de ce cursus');
+  end;
+end;
+
+procedure TForm1.GestUEClick(Sender: TObject);
+begin
+  Clear();
+  FondBleuClair.Visible:=True;
+  case cursus of
+       'TC': begin
+          ListeUETC.Visible:=True ;
+       end; //TC
+       'ISI': begin
+          ListeUEISI.Visible:=True;
+       end; //ISI
+       'RT': begin
+          ListeUERT.Visible:=True;
+       end; //RT
+       'A2I': begin
+          ListeUEA2I.Visible:=True;
+       end; //A2I
+       'MM': begin
+          ListeUEMM.Visible:=True;
+       end; //MM
+       'MTE': begin
+          ListeUEMTE.Visible:=True;
+       end; //MTE
+       'GM': begin
+          ListeUEGM.Visible:=True;
+       end; //GM
+       'GI': begin
+          ListeUEGI.Visible:=True;
+       end; //GI
+  end;
+end;
+
+procedure TForm1.GIbuChange(Sender: TObject);
+begin
+     if Form1.GIbu.Checked then cursus:='GI';
+end;
+
+procedure TForm1.GMbuChange(Sender: TObject);
+begin
+     if Form1.GMbu.Checked then cursus:='GM';
+end;
+
+procedure TForm1.GroupBox1ChangeBounds(Sender: TObject);
+begin
+
+
+end;
+
+procedure TForm1.GroupBox1Click(Sender: TObject);
+begin
+     if Form1.MTEbu.Checked then cursus:='MTE';
+end;
+
+procedure TForm1.Image7Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.ISIbuChange(Sender: TObject);
+begin
+  if ISIbu.Checked then cursus:='ISI';
+end;
+
+procedure TForm1.ListeUEA2IClick(Sender: TObject);
+begin
+
+end;
+
+
 end.
 
