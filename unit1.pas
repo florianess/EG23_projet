bb2238d70b2d5275b4b8c5adf02fbaec96c367f8
Florian Esslinger <florian.esslinger@live.fr>
2018-06-12 17:50:48 +0200

    FINAL COMMIT

--- unit1.pas
+++ unit1.pas
@@ -14,22 +14,34 @@ type
 
   TForm1 = class(TForm)
     A2Ibu: TRadioButton;
-    GestEN: TButton;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
     GestUE: TButton;
+    GestEns: TButton;
     GIbu: TRadioButton;
     GMbu: TRadioButton;
     GroupBox1: TGroupBox;
+    Image10: TImage;
+    Image8: TImage;
+    Image9: TImage;
     ISIbu: TRadioButton;
+    Label10: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
     MTEbu: TRadioButton;
+    NameEns: TLabel;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Panel3: TPanel;
     RepEtu: TButton;
     GestSeme: TButton;
     RTbu: TRadioButton;
+    ScrollBar1: TScrollBar;
+    ScrollBar2: TScrollBar;
     StatsFlux: TButton;
-    ComboBox1: TComboBox;
-    ComboBox2: TComboBox;
-    ComboBox3: TComboBox;
-    ComboBox4: TComboBox;
-    ComboBox5: TComboBox;
     TCbu: TRadioButton;
     TriStatistique: TComboBox;
     Image4: TImage;
@@ -43,7 +55,6 @@ type
     StatistiqueFlux: TPanel;
     TriGestion: TComboBox;
     GestionEtudiantISI: TPanel;
-    Semestre: TComboBox;
     Image1: TImage;
     Image2: TImage;
     Titre: TLabel;
@@ -68,11 +79,8 @@ type
     NF16: TPanel;
     Shape1: TShape;
     FondBleuClair: TShape;
-    procedure ComboBox1Change(Sender: TObject);
-    procedure ComboBox2Change(Sender: TObject);
-    procedure ComboBox3Change(Sender: TObject);
-    procedure ComboBox4Change(Sender: TObject);
-    procedure ComboBox5Change(Sender: TObject);
+    TriPole: TComboBox;
+    procedure GestEnsClick(Sender: TObject);
     procedure GestSemeClick(Sender: TObject);
     procedure GestUEClick(Sender: TObject);
     procedure GIbuChange(Sender: TObject);
@@ -83,10 +91,12 @@ type
     procedure ISIbuChange(Sender: TObject);
     procedure ListeUEA2IClick(Sender: TObject);
     procedure ListeUEISIClick(Sender: TObject);
+    procedure MTEbuChange(Sender: TObject);
     procedure NameFluxClick(Sender: TObject);
     procedure RepEtuClick(Sender: TObject);
     procedure RTbuChange(Sender: TObject);
     procedure A2IbuChange(Sender: TObject);
+    procedure StatistiqueFluxClick(Sender: TObject);
     procedure StatsFluxClick(Sender: TObject);
     procedure TCbuChange(Sender: TObject);
 
@@ -99,7 +109,7 @@ type
 var
   Form1: TForm1;
   indexSemestre : integer;
-  cursus: String;
+  cursus: String = 'TC';
 
 implementation
 
@@ -124,44 +134,17 @@ begin
      Form1.RepartitionISI.Visible:=False;
      Form1.GestionEtudiantISI.Visible:=False;
      Form1.StatistiqueFlux.Visible:=False;
+     Form1.Panel1.Visible:=False;
+     Form1.Panel2.Visible:=False;
+     Form1.Panel3.Visible:=False;
+     Form1.Button4.Visible:=False;
+     Form1.NameEns.Visible:=False;
+     Form1.TriPole.Visible:=False;
+     Form1.ScrollBar2.Visible:=False;
 
 end;
 
-procedure TForm1.ComboBox1Change(Sender: TObject); //GESTION UE
-begin
-  ComboBox2.Text:='Gestion des enseignants';
-  ComboBox3.Text:='Visualisation de la répartition des étudiants';
-  ComboBox4.Text:='Gestion du semestre';
-  ComboBox5.Text:='Statistique des flux';
-  Clear();
-  FondBleuClair.Visible:=True;
-  case ComboBox1.ItemIndex of
-       0: begin
-          ListeUETC.Visible:=True ;
-       end; //TC
-       1: begin
-          ListeUEISI.Visible:=True;
-       end; //ISI
-       2: begin
-          ListeUERT.Visible:=True;
-       end; //RT
-       3: begin
-          ListeUEA2I.Visible:=True;
-       end; //A2I
-       4: begin
-          ListeUEMM.Visible:=True;
-       end; //MM
-       5: begin
-          ListeUEMTE.Visible:=True;
-       end; //MTE
-       6: begin
-          ListeUEGM.Visible:=True;
-       end; //GM
-       7: begin
-          ListeUEGI.Visible:=True;
-       end; //GI
-  end;
-end;
+
 
 procedure TForm1.ListeUEISIClick(Sender: TObject); // Affichage de la fiche de l'UE NF16
 begin
@@ -174,6 +157,11 @@ begin
           end;
        else ShowMessage('Nous n avons pas encore d informations sur cette UE');
   end;
+end;
+
+procedure TForm1.MTEbuChange(Sender: TObject);
+begin
+  if Form1.MTEbu.Checked then cursus:='MTE';
 end;
 
 procedure TForm1.NameFluxClick(Sender: TObject);
@@ -203,6 +191,11 @@ begin
      if Form1.A2Ibu.Checked then cursus:='A2I';
 end;
 
+procedure TForm1.StatistiqueFluxClick(Sender: TObject);
+begin
+
+end;
+
 procedure TForm1.StatsFluxClick(Sender: TObject);
 begin
   Clear();
@@ -217,75 +210,22 @@ begin
    if TCbu.Checked then cursus:='TC';
 end;
 
-procedure TForm1.ComboBox2Change(Sender: TObject); //GESTION ENSEIGNANTS
+
+
+
+
+
+
+
+procedure TForm1.GestEnsClick(Sender: TObject);
 begin
-  ComboBox1.Text:='Gestion des UE';
-  ComboBox3.Text:='Visualisation de la répartition des étudiants';
-  ComboBox4.Text:='Gestion du semestre';
-  ComboBox5.Text:='Statistique des flux';
-
   Clear();
-    case ComboBox2.ItemIndex of
-       0: ; //TC
-       1: ; //ISI
-       2: ; //RT
-       3: ; //A2I
-       4: ; //MM
-       5: ; //MTE
-       6: ; //GM
-       7: ; //GI
-  end;
-end;
-
-
-
-
-procedure TForm1.ComboBox3Change(Sender: TObject); //VISUALISATION REPARTITION ETU
-begin
-  ComboBox1.Text:='Gestion des UE';
-  ComboBox2.Text:='Gestion des enseignants';
-  ComboBox4.Text:='Gestion du semestre';
-  ComboBox5.Text:='Statistique des flux';
-
-  Clear();
-    case ComboBox3.ItemIndex of
-       1: RepartitionISI.Visible:=True; //ISI
-       else ShowMessage('Nous n avons pas encore d informations sur la répartition demandée');
-  end;
-end;
-
-
-
-
-procedure TForm1.ComboBox4Change(Sender: TObject);
-begin  //GESTION DU SEMESTRE
-  ComboBox1.Text:='Gestion des UE';
-  ComboBox2.Text:='Gestion des enseignants';
-  ComboBox3.Text:='Visualisation de la répartition des étudiants';
-  ComboBox5.Text:='Statistique des flux';
-
-  Clear();
-  case ComboBox4.ItemIndex of
-       1: GestionEtudiantISI.Visible:=True ; //ISI
-       else ShowMessage('Nous n avons pas encore d informations sur la gestion de ce cursus');
-  end;
-end;
-
-
-
-
-procedure TForm1.ComboBox5Change(Sender: TObject); //STATISTIQUE DE FLUX
-begin
-  ComboBox1.Text:='Gestion des UE';
-  ComboBox2.Text:='Gestion des enseignants';
-  ComboBox3.Text:='Visualisation de la répartition des étudiants';
-  ComboBox4.Text:='Gestion du semestre';
-
-  Clear();
-    case ComboBox5.ItemIndex of
-       1: StatistiqueFlux.Visible:=True; //ISI
-       else ShowMessage('Nous n avons pas encore les statistiques demandées');
-  end;
+  Form1.Panel1.Visible:=True;
+     Form1.Panel2.Visible:=True;
+     Form1.Panel3.Visible:=True;
+     Form1.Button4.Visible:=True;
+     Form1.NameEns.Visible:=True;
+     Form1.TriPole.Visible:=True;
 end;
 
 procedure TForm1.GestSemeClick(Sender: TObject);
@@ -303,9 +243,11 @@ begin
   FondBleuClair.Visible:=True;
   case cursus of
        'TC': begin
+          ScrollBar2.Visible:=True;
           ListeUETC.Visible:=True ;
        end; //TC
        'ISI': begin
+          ScrollBar2.Visible:=True;
           ListeUEISI.Visible:=True;
        end; //ISI
        'RT': begin
@@ -314,13 +256,12 @@ begin
        'A2I': begin
           ListeUEA2I.Visible:=True;
        end; //A2I
-       'MM': begin
-          ListeUEMM.Visible:=True;
-       end; //MM
        'MTE': begin
+          ScrollBar2.Visible:=True;
           ListeUEMTE.Visible:=True;
        end; //MTE
        'GM': begin
+          ScrollBar2.Visible:=True;
           ListeUEGM.Visible:=True;
        end; //GM
        'GI': begin
@@ -328,6 +269,8 @@ begin
        end; //GI
   end;
 end;
+
+
 
 procedure TForm1.GIbuChange(Sender: TObject);
 begin
@@ -347,7 +290,7 @@ end;
 
 procedure TForm1.GroupBox1Click(Sender: TObject);
 begin
-     if Form1.MTEbu.Checked then cursus:='MTE';
+
 end;
 
 procedure TForm1.Image7Click(Sender: TObject);
